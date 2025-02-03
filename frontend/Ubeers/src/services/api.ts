f79d1bfb73dcff9fef265372ed43e76fc0f7d757
Alexia-N27 <al.nicoleau27@gmail.com>
2025-02-03 18:15:13 +0100

    Affichage des images

--- frontend/Ubeers/src/services/api.ts
+++ frontend/Ubeers/src/services/api.ts
@@ -1,3 +1,4 @@
+import type { Beer } from '@/types/Beer';
 import { useAuth0 } from '@auth0/auth0-vue';
 const API_URL = "http://localhost:3310/api";
 
@@ -5,17 +6,17 @@ export const fetchBeers = async (): Promise<Beer[]> => {
   try {
     const { getAccessTokenSilently } = useAuth0();
     const token = await getAccessTokenSilently();
-    
+
     const response = await fetch(`${API_URL}/beers`, {
       headers: {
         'Authorization': `Bearer ${token}`
       }
     });
-    
+
     if (!response.ok) {
       throw new Error(`Erreur: ${response.status}`);
     }
-    
+
     const data: Beer[] = await response.json();
     return data;
   } catch (error) {
@@ -28,17 +29,17 @@ export const fetchBeerById = async (id: number): Promise<Beer> => {
   try {
     const { getAccessTokenSilently } = useAuth0();
     const token = await getAccessTokenSilently();
-    
+
     const response = await fetch(`${API_URL}/beers/${id}`, {
       headers: {
         'Authorization': `Bearer ${token}`
       }
     });
-    
+
     if (!response.ok) {
       throw new Error(`Erreur: ${response.status}`);
     }
-    
+
     const data: Beer[] = await response.json();
     return data;
   } catch (error) {
